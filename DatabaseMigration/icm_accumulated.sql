5f7b4a28c3b2b60f2aa58fc6563901c0c8472f31
Hyo-Seong <gytjd090792@gmail.com>
2020-09-09 01:02:13 +0900

    icm schema를 생성하고 user 테이블 추가하는 sql 작성함

---
+++ DatabaseMigration/icm_accumulated.sql
@@ -0,0 +1,19 @@
+CREATE SCHEMA `icm`;
+
+
+use `icm`;
+
+
+DROP TABLE IF EXISTS `icm`.`user`;
+/* user table 생성 */
+CREATE TABLE `user` (
+`idx` INT NOT NULL AUTO_INCREMENT,
+`id` VARCHAR(45) NOT NULL PRIMARY KEY,
+`password` VARCHAR(45) NOT NULL,
+`name` VARCHAR(45) NOT NULL,
+`email` VARCHAR(45) NOT NULL,
+`birthday` VARCHAR(45) NOT NULL,
+PRIMARY KEY (`idx`) )
+ENGINE=InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
