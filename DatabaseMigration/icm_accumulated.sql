62bbc2e324d1b98a17413038bb16ebdf67689072
Hyo-Seong <dlgytjd0907@naver.com>
2020-09-09 01:19:08 +0900

    desktop 테이블 뼈대 생성

--- DatabaseMigration/icm_accumulated.sql
+++ DatabaseMigration/icm_accumulated.sql
@@ -12,7 +12,17 @@ CREATE TABLE `user` (
 `password` VARCHAR(45) NOT NULL,
 `name` VARCHAR(45) NOT NULL,
 `email` VARCHAR(45) NOT NULL,
-`birthday` VARCHAR(45) NOT NULL,
+`birthday` VARCHAR(45) NOT NULL
+PRIMARY KEY (`idx`) )
+ENGINE=InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
+
+
+DROP TABLE IF EXISTS `icm`.`desktop`;
+/* user table 생성 */
+CREATE TABLE `desktop` (
+`idx` INT NOT NULL AUTO_INCREMENT
 PRIMARY KEY (`idx`) )
 ENGINE=InnoDB
 DEFAULT CHARACTER SET = utf8mb4
